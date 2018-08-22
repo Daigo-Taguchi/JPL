dce0dcc233d97d5a91deec1d234ff2ccc6a9dee1
DaigoTaguchi <daigo.taguchi@jp.ricoh.com>
2018-08-22 17:22:24 +0900

    時刻がリアルリアルタイムで更新されるように修正。LocalDateTimeを利用して時刻を取得するように修正。Timerクラスの削除。

--- GUI/1-1/src/clock/ClockFrame.java
+++ GUI/1-1/src/clock/ClockFrame.java
@@ -1,6 +1,10 @@
 package clock;
 
 import java.awt.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClockFrame extends Frame{
 	Timer t = new Timer();
@@ -11,7 +15,19 @@ public class ClockFrame extends Frame{
 		addWindowListener(new MyWindowAdapter());
 	}
 	
+	public void RunTimer() {
+		t.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				repaint();
+			}
+		},0, 100);
+	}
+	
 	public void paint(Graphics g) {
-		g.drawString(t.getHour() + ":" + t.getMinute() + ":" + t.getSecond(), 50, 50);
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm:ss");
+		var date = LocalTime.now().format(f);
+		
+		String dateString = date.toString();
+		g.drawString(dateString, 30 , 50);
 	}
 }
--- GUI/1-1/src/clock/Main.java
+++ GUI/1-1/src/clock/Main.java
@@ -3,6 +3,7 @@ package clock;
 public class Main {
 	public static void main(String[] args) {
 		ClockFrame f = new ClockFrame();
-		f.setVisible(true);
+		f.RunTimer();
+		f.setVisible(true);	
 	}
 }
