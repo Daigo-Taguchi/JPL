beba35c0e4bfd986b624dcc379387e39c0f37751
DaigoTaguchi <daigo.taguchi@jp.ricoh.com>
2018-08-23 14:49:40 +0900

    TextFieldとボタンを追加。時計の背景色とフォントを変更。

--- GUI/1-1/src/clock/ClockFrame.java
+++ GUI/1-1/src/clock/ClockFrame.java
@@ -1,18 +1,35 @@
 package clock;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ClockFrame extends Frame{
+public class ClockFrame extends Frame implements ActionListener{
 	Timer t = new Timer();
 	
-	public ClockFrame() {
-		setSize(300,200);
-		setTitle("Application");
+	ClockFrame() {
+		setSize(500,300);
+		setTitle("TaguchiClock");
+		setBackground(Color.BLACK);
+		
+		setLayout(new FlowLayout());
+		TextField t1 = new TextField("フリースペース", 30);
+		add(t1);
+		
+		Button button1 = new Button("押すなよ...絶対に押すなよ");
+		button1.addActionListener(this);
+		add(button1);
+		
+		setResizable(false);
 		addWindowListener(new MyWindowAdapter());
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.exit(0);
 	}
 	
 	public void RunTimer() {
@@ -26,8 +43,11 @@ public class ClockFrame extends Frame{
 	public void paint(Graphics g) {
 		DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm:ss");
 		var date = LocalTime.now().format(f);
-		
 		String dateString = date.toString();
-		g.drawString(dateString, 30 , 50);
+		Font font = new Font("Monospaced", Font.BOLD, 100);
+		
+		g.setFont(font);
+		g.setColor(Color.RED);
+		g.drawString(dateString, 30 , 200);
 	}
 }
