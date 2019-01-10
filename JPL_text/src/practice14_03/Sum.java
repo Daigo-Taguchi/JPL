2355a88cf71fff2c016c634d971b03fd2a21fb70
DaigoTaguchi <daigo.taguchi@jp.ricoh.com>
2019-01-10 19:50:06 +0900

    練習問題修正

--- JPL_text/src/practice14_03/Sum.java
+++ JPL_text/src/practice14_03/Sum.java
@@ -1,14 +1,14 @@
 package practice14_03;
 
 public class Sum {
-	private static int num = 0;
-	
+	private int num = 0;
+
 	/***
 	 * 保持している数値に引数の数値を加算して表示するメソッド
 	 * @param num
 	 */
 	public synchronized void sum(int num) {
-		Sum.num += num;
-		System.out.println(Sum.num);
+		this.num += num;
+		System.out.println(this.num);
 	}
 }
--- JPL_text/src/practice1_15/Main.java
+++ JPL_text/src/practice1_15/Main.java
@@ -1,19 +1,19 @@
-package practice1_15;
-
-public class Main {
-
-	public static void main(String[] args) {
-		SimpleLookup sl = new SimpleLookup();
-		boolean bool1 =  sl.add("taguchi");
-		boolean bool2 = sl.add("taguchi2");
-		
-		var name =  sl.find("taguchi2");
-		System.out.println(name);
-		
-		boolean bool3 = sl.remove("taguchi2");
-		var name2 = sl.find("taguchi2");
-		System.out.println(name2);
-
-	}
-
-}
+//package practice1_15;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		SimpleLookup sl = new SimpleLookup();
+//		boolean bool1 =  sl.add("taguchi");
+//		boolean bool2 = sl.add("taguchi2");
+//
+//		var name =  sl.find("taguchi2");
+//		System.out.println(name);
+//
+//		boolean bool3 = sl.remove("taguchi2");
+//		var name2 = sl.find("taguchi2");
+//		System.out.println(name2);
+//
+//	}
+//
+//}
